a2621d42a71deed21e81dd05c9c6e85b4ee82101
vishnuvardan2004 <150778370+vishnuvardan2004@users.noreply.github.com>
2024-12-21 05:15:09 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
+    const rollno=[61,62,63,68,69,70,64,65,66,67,86]
+let maxElement = rollno[0]
+
+for(let i=0; i<=rollno.length; i++){
+    
+    if(rollno[i]>maxElement){
+        maxElement = rollno[i]
+        
+    }
+}
+console.log(maxElement);
     
 }
-
-module.exports = findLargestElement;
